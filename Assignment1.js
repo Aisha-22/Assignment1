c664952a103b0b76b1b614537bf623e9c9fc2f5f
Aishia <51677946+Aisha-22@users.noreply.github.com>
2019-08-07 19:44:07 +0200

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,19 @@
+//Write out a string to the HTML element.
+console.log("Aisha is", 27);
+
+//The vale should be a random number.
+console.log("5...15".length);
+console.log(Math.floor(Math.random() * (15 - 5) + 5));
+
+//var num1 = "myName";
+
+//var num2 = 27;
+
+//var sum = num1 + num2;
+
+
+
+//Write out a string to the HTML element.
+//function myName(){
+    //document.getElementById("myName").innerHTML = num1 + '+' + num2 + '=' + sum
+//}
